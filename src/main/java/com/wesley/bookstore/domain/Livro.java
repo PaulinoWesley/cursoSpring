51d61a41205724dbee5002d79a9b798444519f37
paulinowesley <wesleypaulinogdl@gmail.com>
2021-09-30 15:25:22 -0300

    Adicionando Validações para Categoria e Livros

--- src/main/java/com/wesley/bookstore/domain/Livro.java
+++ src/main/java/com/wesley/bookstore/domain/Livro.java
@@ -9,6 +9,9 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
+import javax.validation.constraints.NotEmpty;
+
+import org.hibernate.validator.constraints.Length;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
 
@@ -20,8 +23,17 @@ public class Livro implements Serializable{
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY) 
 	private Integer id;
+	
+	@NotEmpty(message = "Campo TITULO é requerido")
+	@Length(min = 1, max = 50, message = "O campo TITULO deve ter entre 1 e 50 caracteres")
 	private String titulo;
+	
+	@NotEmpty(message = "Campo NOME_AUTOR é requerido")
+	@Length(min = 3, max = 100, message = "O campo NOME_AUTOR deve ter entre 3 e 100 caracteres")
 	private String nome_autor;
+	
+	@NotEmpty(message = "Campo TEXTO é requerido")
+	@Length(min = 10, max = 1000000, message = "O campo TEXTO deve ter entre 10 e 1.000.000 caracteres")
 	private String texto;
 
 	@JsonIgnore
--- src/main/java/com/wesley/bookstore/dtos/CategoriaDTO.java
+++ src/main/java/com/wesley/bookstore/dtos/CategoriaDTO.java
@@ -2,6 +2,10 @@ package com.wesley.bookstore.dtos;
 
 import java.io.Serializable;
 
+import javax.validation.constraints.NotEmpty;
+
+import org.hibernate.validator.constraints.Length;
+
 import com.wesley.bookstore.domain.Categoria;
 
 public class CategoriaDTO implements Serializable {
@@ -9,7 +13,13 @@ public class CategoriaDTO implements Serializable {
 	private static final long serialVersionUID = 1L;
 	
 	private Integer id;
+
+	@NotEmpty(message = "Campo NOME é requerido")
+	@Length(min = 3, max = 60, message = "O campo NOME deve ter entre 3 e 60 caracteres")
 	private String nome;
+	
+	@NotEmpty(message = "Campo DESCRIÇÃO é requerido")
+	@Length(min = 4, max = 200, message = "O campo DESCRIÇÃO deve ter entre 4 e 200 caracteres")
 	private String descricao;
 
 	public CategoriaDTO() {
