b1a7f314aa0b01d8638daba12f9293a16907e196
paulinowesley <wesleypaulinogdl@gmail.com>
2021-09-30 11:16:03 -0300

    Implementação metodo Get para Livros e listando todos os livros por categoria

---
+++ src/main/java/com/wesley/bookstore/service/LivroService.java
@@ -0,0 +1,32 @@
+package com.wesley.bookstore.service;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.wesley.bookstore.domain.Livro;
+import com.wesley.bookstore.repositories.LivroRepository;
+import com.wesley.bookstore.service.exceptions.ObjectNotFoundException;
+
+@Service
+public class LivroService {
+
+	@Autowired
+	private LivroRepository repository;
+	
+	@Autowired
+	private CategoriaService categoriaService;
+
+	public Livro findById(Integer id) {
+		Optional<Livro> obj = repository.findById(id);
+		return obj.orElseThrow(
+				() -> new ObjectNotFoundException("Objeto não encontrado :" + id + ", tipo: " + Livro.class.getName()));
+	}
+
+	public List<Livro> findAll(Integer id_cat) {
+		categoriaService.findById(id_cat);
+		return repository.findAllByCategoria(id_cat);
+	}
+}
